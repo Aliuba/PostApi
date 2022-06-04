6fa820c311a434ec29b71d10cfcbcc8b1aadd858
Ataman80 <if190480aar@gmail.comm>
2022-06-04 13:20:41 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
     const [email, setMail] = useState('');
     const [gender, setGender] = useState('male');
     const [users, setUsers] = useState([])
+    const [userID, setUserId] = useState('')
+
 
     function saveUser() {
         console.warn(name, age, email, gender)
@@ -19,26 +21,60 @@ function App() {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
-        })
+        }).then(() => getUsers())
+    }
+
+    function getUsers() {
+        fetch('http://127.0.0.1:8000/users')
+            .then(val => val.json())
+            .then(res => {
+                setUsers(res)
+
+
+            })
+    }
+
+    function upUser() {
+        let data = {name, age, email, gender, userID}
+        fetch(`http://127.0.0.1:8000/users/${userID}`, {
+            method: 'PATCH',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(() => getUsers())
+    }
+
+    function deleteUser(id) {
+        fetch(`http://127.0.0.1:8000/users/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(() => getUsers())
+    }
+
+    function updateUser(id) {
+        let user = users.find(value => value.id === id)
+        setName(user.name)
+        setAge(user.age)
+        setMail(user.email)
+        setGender(user.gender)
+        setUserId(id)
     }
 
     useEffect(() => {
-        fetch('http://127.0.0.1:8000/users')
-            .then(val => val.json())
-            .then(res => setUsers(res))
-    }, [users])
+        getUsers()
+    }, [])
 
     return (
         <div className="App">
-            <h1> Post API </h1>
-            <input type='text' value={name} onChange={(e) => setName(e.target.value)} name='name'/><br/><br/>
-            <input type='number' value={age} onChange={(e) => setAge(e.target.value)} name='age'/><br/><br/>
-            <input type='email' value={email} onChange={(e) => setMail(e.target.value)} name='email'/><br/><br/>
-            <input type='text' value={gender} onChange={(e) => setGender(e.target.value)} name='gender'/><br/><br/>
-            <button type={"button"} onClick={saveUser}>Save New User</button>
-            <h1>Get Users</h1>
 
 
+            <div className='tab'>
+                <h1>Get Users</h1>
                 <table border={'1'}>
                     <thead>
                     <tr>
@@ -47,6 +83,8 @@ function App() {
                         <th>Age</th>
                         <th>Email</th>
                         <th>Gender</th>
+                        <th>Operations</th>
+
                     </tr>
                     </thead>
                     <tbody>
@@ -58,15 +96,29 @@ function App() {
                             <td>{item.age}</td>
                             <td>{item.email}</td>
                             <td>{item.gender}</td>
-                        </tr>
+                            <button onClick={() => deleteUser(item.id)}>Delete</button>
+                            <button onClick={() => updateUser(item.id)}>Update</button>
 
+                        </tr>
                     )
                     }
                     </tbody>
 
                 </table>
 
+            </div>
+            <div>
+                <h1> Post API/ Update API </h1>
+                <input type='text' value={name} onChange={(e) => setName(e.target.value)} name='name'/><br/><br/>
+                <input type='number' value={age} onChange={(e) => setAge(e.target.value)} name='age'/><br/><br/>
+                <input type='email' value={email} onChange={(e) => setMail(e.target.value)} name='email'/><br/><br/>
+                <input type='text' value={gender} onChange={(e) => setGender(e.target.value)} name='gender'/><br/><br/>
+                <button type={"button"} onClick={saveUser}>Save New User</button>
+                <button type={"button"} onClick={upUser}>Update this User</button>
+
+            </div>
         </div>
     );
 }
+
 export default App;
